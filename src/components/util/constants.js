e5e9234b8766ce653146262fb11757dbe39e6529
cntchen <cntchen@gmail.com>
2018-05-24 10:15:09 +0800

    fix: origin host

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -1,8 +1,8 @@
 // 后台接口集合
 const APIS = {
   // Bypassing "Mixed Content" Error in Github Pages
-  ROOT: `${location.protocol}//172.106.33.34/pipeline-node`,
-  PIPLINE: `https://cors-anywhere.herokuapp.com/${location.protocol}//172.106.33.34/pipeline-node/pipeline`,
+  ROOT: `http://172.106.33.34/pipeline-node`,
+  PIPLINE: `https://cors-anywhere.herokuapp.com/http://172.106.33.34/pipeline-node/pipeline`,
   // ROOT: 'http://localhost:7001',
   // PIPLINE: 'http://localhost:7001/pipeline',
 };
