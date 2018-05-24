6a026ec7e9e706c6ddbd1cda33052b8df6df115f
cntchen <cntchen@gmail.com>
2018-05-24 11:17:50 +0800

    fix: give a tip again

--- src/components/util/constants.js
+++ src/components/util/constants.js
@@ -1,7 +1,7 @@
 // 后台接口集合
 const APIS = {
+  ROOT: `${location.protocol}//172.106.33.34/pipeline-node`,
   // Bypassing "Mixed Content" Error in Github Pages
-  ROOT: `http://172.106.33.34/pipeline-node`,
   PIPLINE: `https://cors-anywhere.herokuapp.com/http://172.106.33.34/pipeline-node/pipeline`,
   // ROOT: 'http://localhost:7001',
   // PIPLINE: 'http://localhost:7001/pipeline',
